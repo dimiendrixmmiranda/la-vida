72f494b53473884090d77d203fb09025e525827c
dimiendrixmmiranda <dimiendrixmmiranda@gmail.com>
2025-09-29 15:09:11 -0300

    update

--- src/app/usuario/page.tsx
+++ src/app/usuario/page.tsx
@@ -1,15 +1,72 @@
 'use client'
 import Template from "@/components/template/Template";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
 import useAuth from "@/data/hooks/useAuth";
+import { FaUserAlt, FaBoxOpen, FaCheckCircle, FaMapMarkerAlt, FaTshirt, FaHeadset } from "react-icons/fa";
+import { IoLogOut } from "react-icons/io5";
 
 export default function Page() {
-    const { logout } = useAuth()
-
+    const { logout, usuario } = useAuth()
+    const projects = [
+        {
+            title: "Meu Perfil",
+            description:
+                "Acesse e gerencie suas informações pessoais, atualize dados de contato e visualize seu histórico de atividade.",
+            link: "/",
+            icon: <FaUserAlt className="text-5xl" />
+        },
+        {
+            title: "Pedidos em Aberto",
+            description:
+                "Confira todos os pedidos que ainda estão em andamento, acompanhe o status de cada um e verifique prazos de entrega.",
+            link: "",
+            icon: <FaBoxOpen className="text-5xl" />
+        },
+        {
+            title: "Pedidos Concluídos",
+            description:
+                "Visualize todos os pedidos finalizados, com detalhes de produtos, datas e histórico de compras anteriores.",
+            link: "",
+            icon: <FaCheckCircle className="text-5xl" />
+        },
+        {
+            title: "Adicionar Endereço",
+            description:
+                "Adicione ou atualize endereços de entrega para tornar suas compras mais rápidas e garantir que cheguem ao local correto.",
+            link: "/",
+            icon: <FaMapMarkerAlt className="text-5xl" />
+        },
+        {
+            title: "Preferências de Lavagem",
+            description:
+                "Configure suas preferências de lavagem, como tipo de produto, intensidade de limpeza e cuidados especiais com tecidos.",
+            link: "",
+            icon: <FaTshirt className="text-5xl" />
+        },
+        {
+            title: "Suporte/Chat",
+            description:
+                "Entre em contato com nossa equipe de suporte para tirar dúvidas, resolver problemas e receber atendimento personalizado.",
+            link: "/",
+            icon: <FaHeadset className="text-5xl" />
+        },
+    ]
     return (
         <Template ondaInvertida={false}>
-            <div className="min-h-screen bg-azul-medio flex justify-center items-center text-white">
-                aqui
-                <button onClick={() => logout && logout('/')}>Logout</button>
+            <div className="min-h-screen bg-azul-medio flex justify-center text-white p-4">
+                <div className="mt-[80px] flex flex-col gap-4 xl:gap-8">
+                    <h2 className="uppercase text-3xl text-center xl:text-5xl">Bem vindo <b>{usuario?.nome.split(' ')[0]}</b> <b>{usuario?.nome.split(' ')[usuario?.nome.split(' ').length - 1]}</b></h2>
+                    <div className="max-w-5xl mx-auto">
+                        <HoverEffect items={projects} />
+                    </div>
+                    <button
+                        className="flex justify-center items-center bg-red-500 py-2 text-2xl font-black gap-2"
+                        onClick={() => logout && logout('/')}
+                    >
+                        <IoLogOut />
+                        <p>Logout</p>
+                    </button>
+                </div>
             </div>
         </Template>
     )
---
+++ src/components/ui/card-hover-effect.tsx
@@ -0,0 +1,117 @@
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "motion/react";
+
+import React, { useState } from "react";
+
+export const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: {
+    title: string;
+    description: string;
+    link: string;
+    icon: React.ReactElement
+  }[];
+  className?: string;
+}) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3",
+        className
+      )}
+    >
+      {items.map((item, idx) => (
+        <a
+          href={item?.link}
+          key={item?.link}
+          className="relative group  block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card>
+            <div className="flex flex-col justify-center items-center gap-2 p-2">
+              {item.icon}
+              <CardTitle>{item.title}</CardTitle>
+              <CardDescription>{item.description}</CardDescription>
+            </div>
+          </Card>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-azul border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+      style={{ boxShadow: '0 0 3px 2px black' }}
+    >
+      <div className="relative z-50">
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+export const CardTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h4 className={cn("text-zinc-100 font-bold tracking-wide text-2xl text-center", className)} style={{ textShadow: '1px 1px 2px black' }}>
+      {children}
+    </h4>
+  );
+};
+export const CardDescription = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <p
+      className={cn(
+        "text-zinc-100 tracking-wide leading-relaxed text-sm flex justify-center items-center text-center",
+        className
+      )}
+      style={{ textShadow: '1px 1px 2px black' }}
+    >
+      {children}
+    </p>
+  );
+};
